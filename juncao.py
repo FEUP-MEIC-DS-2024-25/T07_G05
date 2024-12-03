172ec599f5e95b7dc94fb476db9654b2990cb236
ema-12-martins <a97678@alunos.uminho.pt>
2024-12-03 18:33:39 +0000

    ajustes

--- juncao.py
+++ juncao.py
@@ -4,6 +4,14 @@ import sys
 
 def run_shell_script_python():
     # Substitua pelo caminho do seu script shell
+    script_path = "./script_python.sh"  
+    try:
+        subprocess.run(["bash", script_path], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao executar o script: {e}")
+
+def run_shell_script_javascript():
+    # Substitua pelo caminho do seu script shell
     script_path = "./script.sh"  
     try:
         subprocess.run(["bash", script_path], check=True)
@@ -15,8 +23,16 @@ def run_shell_script_python():
 
 if (sys.argv[1]=="python"):
     # Criar a thread
-    thread1 = threading.Thread(target=run_shell_script_python)
+    thread = threading.Thread(target=run_shell_script_python)
     # Iniciar a thread
-    thread1.start()
+    thread.start()
     # Esperar a thread terminar (opcional)
-    thread1.join()
+    thread.join()
+
+elif (sys.argv[1]=="javascript"):
+    # Criar a thread
+    thread = threading.Thread(target=run_shell_script_javascript)
+    # Iniciar a thread
+    thread.start()
+    # Esperar a thread terminar (opcional)
+    thread.join()
